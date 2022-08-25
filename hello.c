8f1c5ae79d7bb61c6b4993dd066db957f7500011
Wes Erickson <wericson4@huskers.unl.edu>
2022-08-25 19:36:25 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Wes Erickson
+ *         Werickson4@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
+Hello World!
